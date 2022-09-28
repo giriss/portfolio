c8af9938ee0cbba058ce4a92b89006055baf0a9d
Girish Gopaul <gg@contractbook.dk>
2022-09-28 10:12:53 +0400

    Added 2 person works

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,49 @@
-import { Header, Segment } from "semantic-ui-react";
+import { Button, Card, Header, Icon } from "semantic-ui-react";
+
+function PortfolioCard({ image, header, description, link, action, actionIcon }) {
+  return (
+    <Card
+      raised
+      image={image}
+      header={header}
+      description={description}
+      extra={
+        <Button
+          basic
+          secondary
+          as="a"
+          target="_blank"
+          href={link}
+        >
+          <Icon name={actionIcon} /> {action}
+        </Button>
+      }
+    />
+  );
+}
 
 function Portfolio() {
   return (
     <>
       <Header as="h2">Stuffs I do for fun...</Header>
-      <Segment.Group>
-        <Segment>
-        </Segment>
-      </Segment.Group>
+      <Card.Group itemsPerRow={2}>
+        <PortfolioCard
+          image="/hideincloud.jpg"
+          header="Hide in Cloud"
+          description="Need to hide files on your phone and keep them securely backed up on the cloud? Hide in Cloud is all you need!"
+          link="https://m.apkpure.com/hide-in-cloud-hide-photo-video-music-doc-etc/me.gopaul.safe_in_cloud"
+          action="View on Play Store"
+          actionIcon="google play"
+        />
+        <PortfolioCard
+          image="/codetoimage.jpg"
+          header="Wanna share code?"
+          description="Have a code snippet you need to share? This app allows you to share code in style!"
+          link="https://github.com/giriss/code-image"
+          action="View on Github"
+          actionIcon="github"
+        />
+      </Card.Group>
     </>
   );
 }
