69e5725deb58f9ce811dae2d8410743c764e6fd5
Girish Gopaul <girish@gopaul.me>
2024-06-10 15:05:38 +0530

    Add vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react"
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
